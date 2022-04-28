1275c4e89a98b1f7311d74ec1bb5bdc1564f6fcb
evreich <vlasov.ms@evrone.com>
2022-04-28 18:13:10 +0300

    Removed spacing for item BaseGrid

--- packages/uikit/src/components/base-grid/BaseGrid.tsx
+++ packages/uikit/src/components/base-grid/BaseGrid.tsx
@@ -6,13 +6,18 @@ const COLUMNS_COUNT = 12;
 export type BaseGridProps = Omit<GridProps, "xs" | "sm" | "md" | "lg" | "xl"> &
   Partial<Record<Theme["breakpoints"]["keys"][0], number>>;
 
-export const BaseGrid: React.FC<BaseGridProps> = (props) => (
-  <Grid
-    {...props}
-    columns={COLUMNS_COUNT}
-    spacing={{
-      mob: 2.5,
-      desk: 3.5,
-    }}
-  />
-);
+export const BaseGrid: React.FC<BaseGridProps> = ({ container, ...props }) => {
+  return (
+    <Grid
+      columns={COLUMNS_COUNT}
+      spacing={{
+        ...(container && {
+          mob: 2.5,
+          desk: 3.5,
+        }),
+      }}
+      container={container}
+      {...props}
+    />
+  );
+};
