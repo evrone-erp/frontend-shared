469829a3adc167c7a9091182637fa4475c11475e
padevone <padevone@gmail.com>
2022-06-27 00:23:32 +0300

    [FIX] Update text button story

--- packages/uikit/src/components/base-text-button/view/StyledIconContainer.tsx
+++ packages/uikit/src/components/base-text-button/view/StyledIconContainer.tsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/material";
 export const StyledIconContainer = styled("div")(
   ({ theme: { spacing } }) => ({
     display: 'inline-flex',
-    alignItems: 'center',
     marginLeft: spacing(2),
+    alignItems: 'center',
   })
 );
