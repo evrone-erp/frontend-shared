c00328f28d038978dc5be0c13a2de1ddfc1b9eed
Olga Popova <popova_oa@evrone.com>
2022-09-07 17:32:45 +0500

    [BK-108] add currency labels and colors https://tracker.yandex.ru/BK-108

--- packages/money/src/currency.ts
+++ packages/money/src/currency.ts
@@ -1,7 +1,25 @@
-export const Currencies = {
-  RUB: "₽",
-  USD: "$",
-  EUR: "€",
-} as const;
+export const CURRENCY_UI_PROPS = {
+  rub: {
+    label: 'Russian Rouble',
+    sign: "₽",
+    color: '#FFB200',
+  },
+  usd: {
+    label: 'US Dollar',
+    sign: "$",
+    color: '#02A0FC',
+  },
+  eur: {
+    label: 'Euro',
+    sign: "€",
+    color: '#4339F2',
+  },
+};
 
-export type Currency = keyof typeof Currencies;
+export type TCurrency = keyof typeof CURRENCY_UI_PROPS;
+
+export type TCurrencyValue = typeof CURRENCY_UI_PROPS[TCurrency];
+
+export const CURRENCIES_LIST = Object.keys(CURRENCY_UI_PROPS) as TCurrency[];
+
+export const CURRENCY_UI_PROPS_VALUES = Object.values(CURRENCY_UI_PROPS) as TCurrencyValue[];
