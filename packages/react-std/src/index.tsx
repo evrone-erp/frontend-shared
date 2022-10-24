f753770f8e1080c5593db8cfbeda33aea684a9c9
idalechin <idalechin@evrone.com>
2022-10-24 13:08:19 +0300

    [MY-31] Added useElementMeasure hook

--- packages/react-std/src/index.tsx
+++ packages/react-std/src/index.tsx
@@ -1,8 +1,9 @@
-export { bindProps, createBindProps } from "./bind-props";
-export { ComposeProviders } from "./compose-providers";
-export { useDebounceValue } from "./use-debounce-value";
-export { useBooleanState } from "./use-boolean-state";
-export { useGetRef } from "./use-get-ref";
-export { useLocalStorage } from "./use-local-storage";
-export { CookiesProvider, useCookie } from "./use-cookie";
-export { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
+export { bindProps, createBindProps } from './bind-props';
+export { ComposeProviders } from './compose-providers';
+export { useDebounceValue } from './use-debounce-value';
+export { useBooleanState } from './use-boolean-state';
+export { useGetRef } from './use-get-ref';
+export { useLocalStorage } from './use-local-storage';
+export { CookiesProvider, useCookie } from './use-cookie';
+export { useIsomorphicLayoutEffect } from './use-isomorphic-layout-effect';
+export { useElementMeasure } from './use-element-measure';
