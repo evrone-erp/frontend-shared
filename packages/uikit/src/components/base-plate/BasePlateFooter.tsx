b9dd9f65dfa96002287b7d9b2da8c09ce0dcacfa
Olga Popova <popova_oa@evrone.com>
2023-05-15 18:02:43 +0500

    [MY-38] fix BaseHelp title type & add primaryActionComment to BasePlateFooter

--- packages/uikit/src/components/base-plate/BasePlateFooter.tsx
+++ packages/uikit/src/components/base-plate/BasePlateFooter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import classNames from 'classnames';
 
 import { BaseComponentProps } from 'design-system/types';
@@ -8,21 +8,27 @@ export type BasePlateFooterProps = {
   primaryAction: JSX.Element;
   secondaryAction?: JSX.Element;
   className?: string;
+  primaryActionComment?: string;
 } & BaseComponentProps;
 
 export const BasePlateFooter: React.FC<BasePlateFooterProps> = ({
   primaryAction,
   secondaryAction,
   className,
+  primaryActionComment,
   ...restProps
 }) => (
   <Box
     display="flex"
     justifyContent="space-between"
+    alignItems="center"
     className={classNames('BasePlateFooter__root', 'BasePlate__footer', className)}
     {...restProps}
   >
-    <Box className="BasePlateFooter__action BasePlateFooter__action--primary">{primaryAction}</Box>
+    <Box display="flex" alignItems="center" className="BasePlateFooter__action BasePlateFooter__action--primary">
+      {primaryAction}
+      {primaryActionComment && <Typography sx={{ marginLeft: '10px' }}>{primaryActionComment}</Typography>}
+    </Box>
     <Box className="BasePlateFooter__action BasePlateFooter__action--secondary">{secondaryAction}</Box>
   </Box>
 );
